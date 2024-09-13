e28052beb6ca261d004b090042b2fcc7742e774f
EMajesty <lassi.laitinen@protonmail.com>
2024-09-13 18:28:13 +0300

    crates gl, glfw added, window created

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate glfw;
+
+use glfw::{Action, Context, Key};
+
+const SCR_WT: u32 = 800;
+const SCR_HT: u32 = 600;
+
 fn main() {
-    println!("Hello, world!");
+    use glfw::fail_on_errors;
+    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
+
+    let (mut window, events) = glfw.create_window(SCR_WT, SCR_HT, "Hell World", glfw::WindowMode::Windowed)
+        .expect("Failed to create window");
+
+    window.make_current();
+    window.set_key_polling(true);
+
+    while !window.should_close() {
+        window.swap_buffers();
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            println!("{:?}", event);
+            match event {
+                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                    window.set_should_close(true)
+                },
+                _ => {},
+            }
+        }
+    }
 }
